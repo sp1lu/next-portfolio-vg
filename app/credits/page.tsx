30383e881a90c5019dd188029cb39a7e212f3bd3
sp1lu <davide.rivolta@yahoo.it>
2025-01-02 18:33:47 +0100

    Main pages

---
+++ app/credits/page.tsx
@@ -0,0 +1,12 @@
+function Credits() {
+    return (
+        <main>
+            <h1>Credits</h1>
+            <ul>
+                <li><p>Credit</p></li>
+            </ul>
+        </main>
+    )
+}
+
+export default Credits
